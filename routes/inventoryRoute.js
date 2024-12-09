bb144cac8b0d35796cda5baa9e32564156cbe0bd
Nidal <nidal@marensolutions.com>
2024-12-09 18:20:45 +0530

    hey

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -4,13 +4,13 @@ const productController = require('../controllers/productController')
 const catControllers=require('../controllers/categoryController')
 
 router.post('/createProduct', productController.addProduct)
-router.get('/findProduct',productController.findProduct)
-router.get('/findAll',productController.findAllProducts)
+router.get('/findOneProduct',productController.findProduct)
+router.get('/findProducts',productController.findAllProducts)
 router.post('/deleteProduct',productController.deleteProduct)
 router.post('/updateProduct',productController.updateProduct)
 router.post('/createCat',catControllers.createCat)
-router.post('/findCat',catControllers.findCat)
-router.post('/findAllCat',catControllers.findAll)
+router.post('/findOneCat',catControllers.findCat)
+router.post('/findCat',catControllers.findAll),
 router.post('/updateCat',catControllers.updateCat)
 
 
